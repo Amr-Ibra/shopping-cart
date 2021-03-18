16f3d9f044fa995531db144669b05564476f98eb
Amr Ibrahim <amribrahim1987@hotmail.com>
2021-03-18 15:36:39 +0100

    Update the CartItems component to: - Take a list of items as props - Render the list of CartItem components

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,9 +1,27 @@
 import React from "react";
+import CartItem from "./CartItem";
 
-const CartItems = () => {
+const CartItems = ({ cartItemsList }) => {
   return (
     <div className="container">
       <h1>Cart Items</h1>
+      <div className="list-group">
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-md-8">Product</div>
+            <div className="col-md-2">Price</div>
+            <div className="col-md-2">Quantity</div>
+          </div>
+        </div>
+        {cartItemsList.map((item) => (
+          <CartItem
+            key={item.product.id}
+            name={item.product.name}
+            price={item.product.priceInCents}
+            quantity={item.quantity}
+          />
+        ))}
+      </div>
     </div>
   );
 };
