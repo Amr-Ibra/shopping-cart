76a8702d12df21e4a7df7a8a3706daa0dfbde947
Amr Ibrahim <amribrahim1987@hotmail.com>
2021-03-18 15:46:55 +0100

    Add the missing pieces to render the app

--- src/App.js
+++ src/App.js
@@ -2,31 +2,15 @@ import "./App.css";
 import CartHeader from "./components/CartHeader";
 import CartFooter from "./components/CartFooter";
 import CartItems from "./components/CartItems";
-
-const cartItemsList = [
-  {
-    id: 1,
-    product: { id: 40, name: "Mediocre Iron Watch", priceInCents: 399 },
-    quantity: 1,
-  },
-  {
-    id: 2,
-    product: { id: 41, name: "Heavy Duty Concrete Plate", priceInCents: 499 },
-    quantity: 2,
-  },
-  {
-    id: 3,
-    product: { id: 42, name: "Intelligent Paper Knife", priceInCents: 1999 },
-    quantity: 1,
-  },
-];
+import cartItemsList from "./components/cartItemsList";
+import currentYear from "./components/currentYear";
 
 function App() {
   return (
     <div>
       <CartHeader />
-      <CartItems />
-      <CartFooter copyright="2016" />
+      <CartItems key={cartItemsList.id} cartItemsList={cartItemsList} />
+      <CartFooter copyright={currentYear} />
     </div>
   );
 }
