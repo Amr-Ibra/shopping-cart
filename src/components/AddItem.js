3de0699de8158e0126b63e0816fefc2d686e8ffd
Amr Ibrahim <amribrahim1987@hotmail.com>
2021-03-22 15:22:47 +0100

    Create the AddItem component

---
+++ src/components/AddItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ProductOption from "./ProductOption";
+
+const AddItem = ({ products, onFormSubmission }) => (
+  <form className="container" onSubmit={onFormSubmission}>
+    <div className="form-group">
+      <label htmlFor="quantity">Quantity</label>
+      <input
+        className="form-control"
+        type="text"
+        id="quantity"
+        name="quantity"
+        required
+      />
+    </div>
+    <div className="form-group">
+      <label htmlFor="products">Products</label>
+      <select className="custom-select" id="products" name="product" required>
+        <option disabled>Select an option...</option>
+        {products.map((product) => (
+          <ProductOption
+            key={product.id}
+            value={product.id}
+            name={product.name}
+          />
+        ))}
+      </select>
+    </div>
+    <input className="btn btn-primary" type="submit" value="Submit" />
+  </form>
+);
+
+export default AddItem;
