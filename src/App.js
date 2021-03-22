7f6ad313d8b9c6780bd97bdd127955fdd0cc2edd
Amr Ibrahim <amribrahim1987@hotmail.com>
2021-03-22 17:44:48 +0100

    Modify the App component to complete the missing pieces: - Change it into a stateful component - Add the initial state, which is the product options + the initial cart items list - Create a function property, which is triggered on form submission - Create a method that updates the state with the new items list

--- src/App.js
+++ src/App.js
@@ -4,12 +4,57 @@ import CartFooter from "./components/CartFooter";
 import CartItems from "./components/CartItems";
 import cartItemsList from "./components/cartItemsList";
 import currentYear from "./components/currentYear";
+import { Component } from "react";
+import AddItem from "./components/AddItem";
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    products: [
+      { id: 40, name: "Mediocre Iron Watch", priceInCents: 399 },
+      { id: 41, name: "Heavy Duty Concrete Plate", priceInCents: 499 },
+      { id: 42, name: "Intelligent Paper Knife", priceInCents: 1999 },
+      { id: 43, name: "Small Aluminum Keyboard", priceInCents: 2500 },
+      { id: 44, name: "Practical Copper Plate", priceInCents: 1000 },
+      { id: 45, name: "Awesome Bronze Pants", priceInCents: 399 },
+      { id: 46, name: "Intelligent Leather Clock", priceInCents: 2999 },
+      { id: 47, name: "Ergonomic Bronze Lamp", priceInCents: 40000 },
+      { id: 48, name: "Awesome Leather Shoes", priceInCents: 3990 },
+    ],
+    items: cartItemsList,
+  };
+
+  onFormSubmission = (e) => {
+    e.preventDefault();
+
+    const id = parseInt(e.target.elements.product.value);
+    const quantity = parseInt(e.target.elements.quantity.value);
+    const productObj = this.state.products.find((product) => product.id === id);
+
+    this.createNewItem(productObj, quantity);
+  };
+
+  createNewItem(productObj, quantity) {
+    const item = { product: productObj, quantity };
+    this.createNewItemsList(item);
+  }
+
+  createNewItemsList(item) {
+    const newItemsList = this.state.items.concat({
+      id: this.state.items.length + 1,
+      product: item.product,
+      quantity: item.quantity,
+    });
+    this.setState({ items: newItemsList });
+  }
+
+  render = () => (
     <div>
       <CartHeader />
-      <CartItems key={cartItemsList.id} cartItemsList={cartItemsList} />
+      <CartItems cartItemsList={this.state.items} />
+      <AddItem
+        products={this.state.products}
+        onFormSubmission={this.onFormSubmission}
+      />
       <CartFooter copyright={currentYear} />
     </div>
   );
